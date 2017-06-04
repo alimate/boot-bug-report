18cac240645a9bcfed4480ba09e3a8a3ab49e2c0
Ali Dehghani <ali.dehghani.g@gmail.com>
2017-06-04 11:22:28 +0430

    Initial code drop
    Signed-off-by: Ali Dehghani <ali.dehghani.g@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Samples for Spring Boot Bug Reports
+This repository will be filled with sample applications in order to reproduce a possible bug or problem
+in Spring Boot. Each branch of this repository will correspond to one specific problem
---
+++ src/main/java/me/alidg/BugReportApplication.java
@@ -0,0 +1,14 @@
+package me.alidg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ali Dehghani
+ */
+@SpringBootApplication
+public class BugReportApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BugReportApplication.class, args);
+    }
+}
